2f93dd47a59b39af16bb0df53955c1848d863e2f
oszmondra <123912358+oszmondra@users.noreply.github.com>
2023-02-26 20:34:31 -0600

    build

--- src/pages/Calificaciones.vue
+++ src/pages/Calificaciones.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="container-fluid">
     <h2 class="text-center font-weight-bold">Calificaciones</h2>
-    <img src="../assets/img/Notas.PNG" alt="Notas" class="mx-auto d-block mt-5" width="900" height="400" > 
+    <img src="../assets/img/Notas.png" alt="Notas" class="mx-auto d-block mt-5" width="900" height="400" > 
   </div>
   </template>
 
