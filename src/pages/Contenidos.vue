3c6201223e5f894c2c0ad36deda8ab35ed5a9eeb
oszmondra <123912358+oszmondra@users.noreply.github.com>
2023-04-08 02:01:54 -0600

    actualización del portafolio

--- src/pages/Contenidos.vue
+++ src/pages/Contenidos.vue
@@ -36,11 +36,9 @@
     <p>Las bibliotecas de JavaScript son colecciones de fragmentos de código escritos previamente que los
        desarrolladores web pueden volver a utilizar para realizar funciones estándar de JavaScript. Las bibliotecas
        son útiles para los desarrolladores porque pueden ahorrar tiempo y esfuerzo al escribir código repetitivo. Algunas
-       de las bibliotecas de JavaScript más populares incluyen jQuery, React, Angular y Vue.js.</p>
-      </div> 
+       de las bibliotecas de JavaScript más populares incluyen jQuery, React, Angular y Vue.js.</p>     
       <p></p>
 
-    <div class="justify-text-container">
     <p></p>
     <h4>Semana 2 - Bootstrap-Vuejs</h4>
     <p></p>
@@ -98,23 +96,19 @@
        justo al cargarse o al destruirse.</li>
     <li><strong>Aplicaciones SPA: </strong>VueJS tiene un paquete especial para tener un sistema de rutas y construir
        aplicaciones de una sola página.</li>
-      </div>
       <p></p>
-      <div class="justify-text-container">
     <p></p>
     <h4>Semana 3 - Rendering declarativo_Directivas.</h4>
-   <p>El rendering declarativo es una técnica utilizada para describir cómo debería ser el resultado final de una página
+    <p>El rendering declarativo es una técnica utilizada para describir cómo debería ser el resultado final de una página
        web en lugar de cómo se debe llegar a ese resultado. En otras palabras, se define la apariencia final deseada y 
        Vue.js se encarga de calcular los cambios necesarios para llegar a ese resultado final</p>
-   <p>Por otro lado, las directivas son una característica importante de Vue.js que permiten conectar elementos del DOM 
+    <p>Por otro lado, las directivas son una característica importante de Vue.js que permiten conectar elementos del DOM 
        con el modelo de datos de la aplicación. Las directivas se utilizan para aplicar ciertas acciones a elementos HTML,
        como la asignación de valores, el control de eventos y la modificación de estilos. Algunas de las directivas más 
        utilizadas en Vue.js incluyen v-bind, v-if, v-for y v-on.</p>
-   <p>En resumen, el rendering declarativo y las directivas son herramientas fundamentales que permiten a los 
+    <p>En resumen, el rendering declarativo y las directivas son herramientas fundamentales que permiten a los 
        desarrolladores de Vue.js crear interfaces de usuario dinámicas y reactivas de forma más sencilla y eficiente.</p>
-  </div>
-  <p></p>
-  <div class="justify-text-container">
+    <p></p>
     <p></p>
     <h4>Semana 4 - Eventos_Clases.</h4>
     <p></p>
@@ -134,8 +128,154 @@
     <p>Las clases también se pueden agregar directamente con la sintaxis :class="{className: condition}", lo que es lo 
       mismo que v-bind:class="{className: condition}". Además, se pueden agregar varias clases usando un objeto o un 
       arreglo</p>
-    </div>
+    <p></p>
+    <h4>Semana 5 - Componentes</h4>
+    <p></p>
+    <p>Un componente en Vue.js es la forma de reutilizar una instancia de Vue tantas veces como sea necesario. Mediante 
+      los componentes se pueden crear elementos propios html, cada uno con sus propiedades y métodos particulares que
+      fácilmente se pueden reutilizar en toda la aplicación.</p>
+    <p></p>
+    <h6>Sintaxis para creación de componentes</h6>
+      <b>vue.component("name", { options })</b>
+    <p><li><b>name</b> es el nombre del componente</li>
+      <li><b>{ options }</b> son todas aquellas opciones que se pueden enviar a una instancia de Vue con dos pequeñas
+         diferencias las cuales son las siguientes:</li> 
+         <li>no es necesario especificar el elemento contexto { el: '#app’ }</li>
+         <li>data ahora es una función que retorna un objeto.</li></p>
+    <p></p>
+    <h4>Semana 6 </h4>
+    <h2>EVALUACIONES PARCIALES</h2>
+    <p></p>
+    <h4>Semana 7 - Introducción al desarrollo con php</h4>
+    <img src="../assets/img/phplogo.png" alt="phplogo" class="mx-auto d-block mt-5" width="200" height="180" >
+    <p></p>
+    <h6>PHP:</h6>
+    <p></p>
+    <p>PHP (acrónimo recursivo de PHP: Hypertext Preprocessor) es un lenguaje de programación de código abierto 
+      Es la expresión con la que se conoce al software o programas que se pueden usar libremente. Normalmente son
+      gratuitos y se puede acceder a su código fuente. muy popular especialmente adecuado para el desarrollo
+      web y que puede ser incrustado dentro de código HTML.</p>   
+    <p>El origen de PHP se remonta a 1995 cuando Rasmus Lerdorf, desarrollo el producto PHP/FI, el cual consistía 
+      simplemente en un conjunto de scripts de Perl a los que llamó ‘Personal Home Page Tools’. Ramus Lerdorf en 
+      vista de la necesidad de dotar de mayor funcionalidad a sus scripts, fue escribiendo una mayor implementación 
+      utilizando C que permitía la comunicación con Bases de Datos y el desarrollo de aplicaciones web dinámicas.</p>
+    <p></p>
+    <h6>Cómo funciona PHP:</h6>
+    <p></p>
+    <p>El usuario escribe una URL una ruta o dirección para poder localizar una página web, imagen, servidor etc. 
+      En su navegador y éste envía un petición al servidor web. Un servidor web es la maquina o computador donde se
+      almacena una página web. El servidor lee la página PHP (la interpreta). Envía al navegador del usuario un 
+      documento compilado en formato HTML</p>
+    <p></p>
+    <h6>¿Qué puede hacer?</h6>
+    <p></p>
+    <li>Permite la ejecución de scripts en la parte del servidor. </li>
+    <li>Se necesitan tres elementos: El analizador PHP (CGI ó módulo), un servidor web y un navegador. </li>
+    <li>Ejecución de scripts desde la línea de comandos sin la necesidad de un servidor Web o navegador. </li>
+    <li>Escritura de aplicaciones gráficas clientes por medio de una extensión llamada PHP-GTK. </li>
+    <li>Puede ser utilizado en cualquiera de los principales sistemas operativos del mercado. </li>
+    <li>Linux, Unix, Microsoft Windows, Mac OS X, RISC OS y probablemente alguno más. </li>
+    <li>Soporta la mayoría de los servidores web de hoy en día. </li>
+    <li>Incluyendo Apache, Microsoft Internet Information Server, Personal Web Server, y muchos otros </li>
+    <p></p>
+    <h4>Semana 8 - Loops, Arrays y functions </h4>
+    <p></p>
+    <h6>Loops</h6>
+    <p></p>
+    <p>Un bucle en PHP se usa para ejecutar una declaración o un bloque de declaraciones, varias veces hasta que
+       se cumpla una condición específica.</p>
+    <p>PHP soporta:</p>
+    <li>for loop</li>
+    <li>while loop</li>
+    <li>do-while loop</li>
+    <li>foreach loop</li>
+    <p></p>
+    <b>FOR</b>
+    <p>Este tipo de bucles se utiliza cuando el usuario sabe de antemano cuántas veces debe ejecutarse el bloque.
+      Es decir, el número de iteraciones se conoce de antemano. Este tipo de bucles también se conocen como bucles controlados
+      por entrada.Hay tres parámetros principales para el código: la inicialización, la condición de prueba y el contador.</p>
+    <p></p>
+      <b>WHILE</b>
+    <p>El ciclo while también es un ciclo de control de entrada como para los ciclos, es decir, primero verifica la condición al
+      comienzo del ciclo y, si es verdadero, ingresa al ciclo y ejecuta el bloque de instrucciones, y continúa ejecutándolo como
+      siempre y cuando la condición sea cierta. </p>
+    <p></p>
+    <b>DO-WHILE</b>
+    <p>Es un bucle de control de salida, lo que significa que primero ingresa al bucle, ejecuta las declaraciones y luego verifica 
+      la condición. Por lo tanto, una declaración se ejecuta al menos una vez al usar el bucle do ... while. Después de ejecutar
+      una vez, el programa se ejecuta siempre que la condición sea verdadera.</p>
+    <p></p>
+    <b>FOREACH</b>
+    <p>Este bucle se utiliza para iterar sobre matrices. Para cada contador de bucle, se asigna un elemento de matriz y el 
+      siguiente contador se desplaza al siguiente elemento. </p>
+    <p></p>
+    <h6>Array</h6>
+    <p></p>
+    <p>Las matrices en PHP son un tipo de estructura de datos que nos permite almacenar múltiples elementos de un tipo de 
+      datos similar en una sola variable, lo que nos ahorra el esfuerzo de crear una variable diferente para cada dato. Los
+      arreglos son útiles para crear una lista de elementos de tipos similares, a los que se puede acceder utilizando su índice
+      o clave.</p>
+    <p>Básicamente, hay tres tipos de matrices en PHP:</p>
+    <li><b>Matrices indexadas o numéricas:</b> una matriz con un índice numérico donde los valores se almacenan linealmente.</li>
+    <li><b>Matrices asociativas:</b> una matriz con un índice de cadena donde en lugar de almacenamiento lineal, a cada valor
+      se le puede asignar una clave específica.</li>
+    <li><b>Matrices multidimensionales:</b> una matriz que contiene una matriz única o múltiple dentro de ella y se puede acceder 
+      a través de múltiples índices.</li>
+    <p></p>
+    <h4>Semana 9 - Introducción a POO</h4>
+    <p></p>
+    <p>La Programación Orientada a Objetos (POO u OOP según sus siglas en inglés) es un paradigma de programación que usa
+      objetos y sus interacciones para diseñar aplicaciones y programas de computadora. Está basado en varias técnicas,
+      incluyendo herencia, modularidad, polimorfismo, y encapsulamiento. Su uso se popularizó a principios de la década
+      de 1990. Actualmente son muchos los lenguajes de programación que soportan la orientación a objetos. 
+      El término de Programación Orientada a Objetos indica más una forma de diseño y una metodología de desarrollo de 
+      software que un lenguaje de programación, ya que en realidad se puede aplicar el Diseño Orientado a Objetos (En inglés 
+      abreviado OOD, Object Oriented Design), a cualquier tipo de lenguaje de programación.</p>
+    <h6>Abstracción</h6>
+    <li>La abstracción es un proceso de interpretación y diseño que implica reconocer y enfocarse en las características 
+      importantes de una situación u objeto, y filtrar o ignorar todas las particularidades no esenciales.</li>
+    <li>Dejar a un lado los detalles de un objeto y definir las características específicas de éste, aquellas que lo distingan 
+      de los demás tipos de objetos.</li>
+    <li>Hay que centrarse en lo que es y lo que hace un objeto, antes de decidir cómo debería ser implementado.</li>     
+    <li>Se hace énfasis en el qué hace más que en el cómo lo hace.</li>
+    <p></p>
+    <h6>Encapsulamiento</h6>
+    <li>Es la propiedad que permite asegurar que la información de un objeto está oculta del mundo exterior.</li>
+    <li>El encapsulamiento consiste en agrupar en una Clase las características(atributos) con un acceso privado y los
+      comportamientos (métodos) con un acceso público.</li>
+    <li>Acceder o modificar los miembros de una clase a través de sus métodos.</li>
+    <p></p>
+    <h6>Herencia</h6>
+    <li>Es una propiedad que permite crear clases a partir de otras ya existentes, obteniendo características (métodos y atributos)
+      similares a los ya existentes.</li>
+    <li>En otras palabras, la nueva clase (subclase o clase derivada) puede utilizar la misma implementación de su superclase 
+      (clase base) o especificar una nueva implementación.</li>
+    <p></p>
+    <h4>Semana 10 - Clases Abstratas e Interfaces</h4> 
+    <p></p>
+    <h6>Clases abstractas</h6>
+    <li>Las clases abstractas no pueden ser instanciadas.</li>
+    <li>Pueden declarar la existencia de los métodos, pero no su implementación.</li>
+    <li>Puede contener métodos no-abstractos, pero debe de contener al menos un método abstracto.</li>
+    <li>Los métodos abstractos deben ser definidos en las clases heredadas, pero deben tener la misma visibilidad.</li>
+    <li>La implementación debe tener la misma “firma”: la declaración de tipos y el número de argumentos.</li>
+    <li>Las clases abstractas están implementadas desde la versión 5.4.0.</li>
+    <p></p>
+    <h6>Interfaces</h6>
+    <li>Las interfaces nos permiten crear código con el cual especificar qué métodos deben ser implementados
+      por una clase (prototipos), sin definir su implementación.</li>
+    <li>Las interfaces se definen con la palabra reservada interface.</li>
+    <li>Los métodos declarados en una interfaz deben ser públicos.</li>
+    <li>Para implementar una interface desde una clase se utiliza la palabra reservada implements.</li>
+    <li>Una clase puede implementar más de una interface si se desea.</li>
 
+
+
+
+
+
+
+   </div>
 </template>
 
 <script>
